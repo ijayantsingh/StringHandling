a8c231f3111ac00eda51a16d5507687103ae74b3
Jayant Singh <jayant.singh_cs18@gla.ac.in>
2019-10-05 09:51:18 +0530

    Add files via upload
    ques 3

--- Main.java
+++ Main.java
@@ -1,17 +1,14 @@
 package com.company;
 
-
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter the String");
+        System.out.println("Enter the String: ");
         String a = sc.next();
-        System.out.println("enter the substrng index: ");
-        int b = sc.nextInt();
-        String c = a.substring(b);
-        System.out.println(c);
-
+        String b = a.replace("a", "*");
+        System.out.println(b);
     }
 }
